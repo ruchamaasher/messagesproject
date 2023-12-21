770f2b50c7a6cb81ac3973b541e4c1a8aeeecf9e
Ruchama Asher <96139002+ruchamaasher@users.noreply.github.com>
2023-12-21 23:12:26 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace bll
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ myMessages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll
+{
+   
+        public class CustomMessage
+    {
+        public string Body { get; set; }
+        public string Id { get; set; }
+        public string From { get; set; }
+            public string Subject { get; set; }
+
+        // public DateTimeOffset? ReceivedDateTime { get; set; }
+        public string ReceivedDateTime { get; set; }
+
+    }
+
+    }
+
+
